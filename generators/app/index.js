b5ae0b25d2fbca794cac94163498eea98837faa5
Josh Nichols <technicalpickles@github.com>
2014-08-22 23:14:03 -0400

    don't bower

--- generators/app/index.js
+++ generators/app/index.js
@@ -37,7 +37,6 @@ var HubotGenerator = yeoman.generators.Base.extend({
       this.dest.mkdir('app/templates');
 
       this.src.copy('_package.json', 'package.json');
-      this.src.copy('_bower.json', 'bower.json');
     },
 
     projectfiles: function () {

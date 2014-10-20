75922d05a0e98ff9b266f3436b12a40fd1adc467
Josh Nichols <technicalpickles@github.com>
2014-10-20 07:24:51 -0400

    tweak grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,28 @@
 'use strict';
 
 module.exports = function(grunt) {
+
   grunt.loadNpmTasks('grunt-release');
 
   grunt.initConfig({
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          require: 'coffee-script'
+        },
+        src: ['test/**/*.coffee']
+      }
+    },
     release: {
       options: {
         tagName: 'v<%= version %>',
         commitMessage: 'Prepared to release <%= version %>.'
       }
+    },
+    watch: {
+      files: ['Gruntfile.js', 'test/**/*.coffee'],
+      tasks: ['test']
     }
   });
 };

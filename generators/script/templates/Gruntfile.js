284aa9c5a692d2ee136738e3073c1f749c1f0d78
MrSaints <ian@fyianlai.com>
2014-10-26 20:45:52 +0000

    Fixed Mocha for CoffeeScript 1.7+

--- generators/script/templates/Gruntfile.js
+++ generators/script/templates/Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
       test: {
         options: {
           reporter: 'spec',
-          require: 'coffee-script'
+          require: 'coffee-script/register'
         },
         src: ['test/**/*.coffee']
       }

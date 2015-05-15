dc8983e252bb85a44fc8ddce46c8d2ebbc4e8e90
David Kassa <david.kassa@gmail.com>
2015-05-15 08:39:31 -0500

    Set path to sync hubot.cmd with hubot bash script
    This fixes issues with coffee-script not being installed globally and potentially other binary conflicts.

--- generators/app/templates/bin/hubot.cmd
+++ generators/app/templates/bin/hubot.cmd
@@ -1,3 +1,7 @@
 @echo off
 
+call npm install
+SETLOCAL
+SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
+
 npm install && node_modules\.bin\hubot.cmd --name "<%= botName %>" %* 

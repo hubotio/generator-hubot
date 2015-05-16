3fa5769bbe5628ccc58e30004e06222171723f73
David Kassa <david.kassa@gmail.com>
2015-05-15 22:17:45 -0500

    remove redundant npm install call

--- generators/app/templates/bin/hubot.cmd
+++ generators/app/templates/bin/hubot.cmd
@@ -4,4 +4,4 @@ call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 
-npm install && node_modules\.bin\hubot.cmd --name "<%= botName %>" %* 
+node_modules\.bin\hubot.cmd --name "<%= botName %>" %* 

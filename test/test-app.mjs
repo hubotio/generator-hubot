a05d94000d3eee6098f1e4b26c438431eaa1965b
Joey Guerra <github@joeyguerra.com>
2024-04-01 11:43:53 -0500

    fix: Passed in options are not being set when using --defaults

--- test/test-app.mjs
+++ test/test-app.mjs
@@ -3,16 +3,16 @@ import { describe, it, before } from 'node:test'
 import path from 'node:path'
 import helpers from 'yeoman-test'
 import assert from 'yeoman-assert'
-
+import fs from 'node:fs/promises'
 const __dirname = path.dirname(new URL(import.meta.url).pathname)
 
 describe('hubot:app', function () {
   before(async () => {
     await helpers.run(path.join(__dirname, '../generators/app'))
-      .withOptions({ 'skip-install': true })
+      .withOptions({ 'skip-install': true, defaults: true, name: 'hubot', owner: 'temp' })
   })
 
-  it('creates files', function () {
+  it('creates files', async function () {
     assert.file([
       'bin/hubot',
       'bin/hubot.cmd',
@@ -23,5 +23,7 @@ describe('hubot:app', function () {
       'package.json',
       'scripts/Example.mjs'
     ])
+    assert.fileContent('bin/hubot', await fs.readFile(path.join(__dirname, '../test/temp/bin/hubot'), 'utf8'))
+    assert.fileContent('package.json', await fs.readFile(path.join(__dirname, '../test/temp/package.json'), 'utf8'))
   })
 })
